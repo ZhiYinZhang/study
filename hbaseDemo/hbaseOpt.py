4f5195ff4a4aab37f8caeaf5dcec8ee96d31242d
ZhiYinZhang <2454099127@qq.com>
2019-03-31 22:38:29 +0800

    3/31

--- hbaseDemo/hbaseOpt.py
+++ hbaseDemo/hbaseOpt.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # datetime:2019/1/9 16:00
+from pyspark.sql import SparkSession
+from pyspark import SparkContext,RDD
+
+host="entrobus12,entrobus32,entrobus34"
+table="test_ma"
+conf={"hbase.zookeeper.quorum":host,
+       "hbase.zookeeper.property.clientPort":"2181",
+       "hbase.mapreduce.inputtable":table}
+
+keyC = "org.apache.spark.examples.pythonconverters.ImmutableBytesWritableToStringConverter"
+valC = "org.apache.spark.examples.pythonconverters.HBaseResultToStringConverter"
+classPath="E:\dataset\hbase_jar\*"
+spark:SparkSession=SparkSession.builder.master("local[2]")\
+    .config("spark.driver.extraClassPath",classPath)\
+    .config("spark.executor.extraClassPath",classPath)\
+    .appName("read_hbase").getOrCreate()
+sc:SparkContext=spark.sparkContext
+
+hbase_rdd:RDD = sc.newAPIHadoopRDD(
+"org.apache.hadoop.hbase.mapreduce.TableInputFormat",
+"org.apache.hadoop.hbase.io.ImmutableBytesWritable",
+"org.apache.hadoop.hbase.client.Result",
+keyConverter=keyC,
+valueConverter=valC,
+conf=conf)
+
+hbase_rdd.foreach(lambda x:print(x))
+
